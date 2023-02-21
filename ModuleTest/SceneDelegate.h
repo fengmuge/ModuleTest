130a228b26fb6c30e6abeb529ff6b19453c45531
miaopeigen <miaopeigen@v1.cn>
2023-02-21 15:24:11 +0800

    Initial Commit

---
+++ ModuleTest/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ModuleTest
+//
+//  Created by admin on 2023/2/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ModuleTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ModuleTest
+//
+//  Created by admin on 2023/2/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
